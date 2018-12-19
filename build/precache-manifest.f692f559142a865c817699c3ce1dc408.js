04aa9b50945fd318cf8f3dd98ad85ed79f4bdeb3
70528 <705283729@qq.com>
2018-12-19 08:54:39 +0800

    add build

---
+++ build/precache-manifest.f692f559142a865c817699c3ce1dc408.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1478bd36712a7664389f",
+    "url": "/static/js/runtime~main.1478bd36.js"
+  },
+  {
+    "revision": "5a61c09505c736493b13",
+    "url": "/static/js/main.5a61c095.chunk.js"
+  },
+  {
+    "revision": "321f2c1a722e7e38efb1",
+    "url": "/static/js/6.321f2c1a.chunk.js"
+  },
+  {
+    "revision": "343b14cde8565a8fc60e",
+    "url": "/static/js/5.343b14cd.chunk.js"
+  },
+  {
+    "revision": "c1e250c2df27d74b89e5",
+    "url": "/static/js/4.c1e250c2.chunk.js"
+  },
+  {
+    "revision": "b81cf6865e05738d8065",
+    "url": "/static/js/3.b81cf686.chunk.js"
+  },
+  {
+    "revision": "e150ff2db5e995fe25e9",
+    "url": "/static/js/2.e150ff2d.chunk.js"
+  },
+  {
+    "revision": "4b4977114184b9ff920e",
+    "url": "/static/js/1.4b497711.chunk.js"
+  },
+  {
+    "revision": "343b14cde8565a8fc60e",
+    "url": "/static/css/5.11213f2f.chunk.css"
+  },
+  {
+    "revision": "e1c02a75b9ebd98af45b85a774407f96",
+    "url": "/index.html"
+  }
+];
